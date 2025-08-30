318fc1a9e5f7ac3968761fbee26bd5d9ab13ebf0
mahendra7041 <chavdamahendra247@gmail.com>
2025-08-31 00:58:37 +0530

    refactor: structure and express-inertia version updated

---
+++ examples/react/configs/session.config.js
@@ -0,0 +1,11 @@
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET || "secret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    secure: process.env.NODE_ENV === "production",
+  },
+};
+
+export default sessionConfig;
